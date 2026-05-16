c60ef113eba7fbd2260fa6e23119b65fd003493a
RyanDeBarros <ryan2004db@gmail.com>
2026-05-16 12:21:26 -0400

    Cleaned up draw cursor blueprint

--- Source/GameDevTVJam/Private/Player/Trajectory.cpp
+++ Source/GameDevTVJam/Private/Player/Trajectory.cpp
@@ -2,26 +2,3 @@
 
 
 #include "Player/Trajectory.h"
-
-// Sets default values
-ATrajectory::ATrajectory()
-{
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
-}
-
-// Called when the game starts or when spawned
-void ATrajectory::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void ATrajectory::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
